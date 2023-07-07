028e975556f659b5928ca4e73e72fded5ad9ac0a
shenjinxin <shenjinxin@guazi.com>
2023-07-07 11:35:39 +0800

    lab01: Completed Arithmetic.java

--- lab01/src/Arithmetic.java
+++ lab01/src/Arithmetic.java
@@ -20,7 +20,7 @@ public class Arithmetic {
      * @return Sum of a and b
      * */
     public static int sum(int a, int b) {
-        return a * b;
+        return a + b;
     }
 
     public static void main(String[] args) {
